c535cb698e342e10648308898e9166c3da662071
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-13 00:59:00 +0300

    created the router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [];
 
