e94f73d73be7219cbaa2a79bb01d75001628e4b7
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-12 10:44:30 +0300

    created a constructor in the profile class

--- src/app/profile.ts
+++ src/app/profile.ts
@@ -1,2 +1,10 @@
 export class Profile {
+    showDescription:boolean;
+    constructor(
+        public username:string,
+        public profilephotos: any,
+        public findprofile: string ){
+        this.showDescription=false;
+     
+     }
 }
